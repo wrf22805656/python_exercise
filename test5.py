8ae0989abc0435c8f3694df5c43dbdff1bcc1d6e
Ruifeng Wang <wrf22805656@users.noreply.github.com>
2016-05-11 19:48:38 -0600

    Python programming book, chapter 3 test

---
+++ test5.py
@@ -0,0 +1,38 @@
+#!usr/bin/python
+
+# question 1
+print 'Comp''Sci'
+print 'Computer'+'Sciencer'
+print 'H20'*3
+print 'C02'*0
+
+# question 2
+print "They'll hibernate during the winter"
+print '"Absolutely not," he said'
+print '''"He said, 'Absolutely not,"' recalled Mel.'''
+print "hydrogen sulfide"
+print 'left\\right'
+
+
+# question 5
+x = 3
+y = 12.5
+
+print "The rabbit is %d." % x
+print "The rabbit is %d years old." % x
+print "%f is average." %y
+print "%f*%d" % (y,x)
+print "%f*%d is 37.5" % (y,x)
+
+
+
+# question 7
+num = float(raw_input("please enter your fking name:"))
+
+# question 8
+'abc' 'def'
+left = 'abc'
+right = 'def'
+
+print left + right 
+
