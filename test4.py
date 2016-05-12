879ec76e052cff5f96d15efc3770dbc6e5a74127
Ruifeng Wang <wrf22805656@users.noreply.github.com>
2016-05-11 18:12:02 -0600

    Python programming book, chapter 2 test

---
+++ test4.py
@@ -0,0 +1,51 @@
+#usr/bin/python
+
+def abc(time):
+    a = 100
+    b = 100
+    d = time + a + b
+    return time + a + b
+print abc(100)
+
+
+# question 1 
+print 9-3
+print 8*2.5
+print 9/2
+print 9/-2
+print 9%2
+print 9%-2
+print -9%2
+print 9/-2.0
+print 4+3*5
+print (4+3)*5
+
+
+
+# question 2 ok
+
+# question 3 
+temp = 24
+def cel(temp):
+    return temp*1.8+32
+temp = cel(temp)
+print temp
+
+
+
+## question 4
+x = 10.5
+y = 4
+x = x+y
+print x,y
+
+
+# question 5
+def function(x):
+    x+=x-x
+    return x
+print function(3)
+
+
+# question 6
+
